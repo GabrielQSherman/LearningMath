d7a022f7f3b555af84f21273d0b93178c264536b
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-10-28 19:30:58 -0400

    Add files via upload

---
+++ flowervotexsimple.js
@@ -0,0 +1,56 @@
+
+
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight;
+       
+
+   context.translate((width / 2), height /2);
+
+var time = 0;
+window.requestAnimationFrame(function loop() {
+    time++;
+    context.save();
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.restore();
+    
+
+  context.rotate(Math.PI / 10 );
+
+for (let i = 0; i < 10000; i++) {
+  
+    context.strokeStyle = 'hsl(' + (time*5) + ', 80%, 30%)';
+    context.beginPath();
+    
+    ////////////////////////////
+    context.moveTo(i+10,0);
+
+    context.lineTo(width,height);
+    /////////////////////////////
+
+    context.stroke();
+   context.rotate(Math.PI/100)
+
+   //context.strokeStyle = 'hsl(' + (time) + ', 80%, 50%)';
+   context.beginPath();
+
+   //////////////////////////////////
+   context.moveTo(i,400);
+
+   context.lineTo(width,0);
+   /////////////////////////////////
+
+   context.stroke();
+  context.rotate(.3)
+   
+
+}
+
+     
+setTimeout(window.requestAnimationFrame,0, (loop));  
+})      
+}
+
