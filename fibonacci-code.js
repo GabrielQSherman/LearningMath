9f46cc6d9909d366d7488b6f7b7458fca512dde2
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-16 15:41:52 -0500

    Add files via upload

---
+++ fibonacci-code.js
@@ -0,0 +1,58 @@
+window.onload = function () {
+     var canvas = document.getElementById("canvas"),
+        context = canvas.getContext("2d"),
+        width = canvas.width = this.innerWidth, //this refers to window
+        height = canvas.height = this.innerHeight,
+        time = 0;
+
+
+
+    context.translate(width / 2, height / 2);
+    context.strokeStyle = "black";
+        
+// let fibSequence = [], curfibNum = 1, prevFibnum = 0;
+//                 for (let i = 0; i < num; i++) {
+//                     if (prevFibnum + curfibNum > num) {
+//                         fibSequence.push(curfibNum);
+//                         i = num;
+//                     } else {
+//                         fibSequence.push(curfibNum);
+//                         curfibNum = prevFibnum + curfibNum;
+//                         prevFibnum = fibSequence[i];
+
+
+
+
+    var fibonacci = 0,
+    prv = 0
+    next = .1;
+
+    render();
+    function render() {
+    var fibonacci = (prv + next)/1000,
+        prv = next,
+        next = fibonacci,
+        circley = Math.sin(time),
+        circlex = Math.cos(time);
+
+
+
+        time++
+        context.beginPath();
+        context.moveTo(circlex * 1000,circley * 1000);
+        context.lineTo(fibonacci,0);
+        context.strokeStyle = "orange";
+        context.stroke();
+
+
+        // context.beginPath();
+        // context.moveTo(0,fibonacci);
+        // context.lineTo(fibonacci,0);
+        // context.strokeStyle = "orange";
+        // context.stroke();
+       
+
+        requestAnimationFrame(render);
+    }
+
+}
